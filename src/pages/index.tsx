e1b3283c5b5ac3c0d7e4b025908c8c1b30944417
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2023-03-01 14:42:43 +0100

    working on embed code generator done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import EmbedCodeGenerator from "../components/EmbedCodeGenerator";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const IndexPage: React.FC<PageProps> = () => {
 	const componentUrl = "paste your component link here";
@@ -9,7 +9,7 @@ const IndexPage: React.FC<PageProps> = () => {
 
 	return (
 		<Box sx={{ width: "50%", mx: "auto"}}>
-			<h1>Welcome to ForkFacts!</h1>
+			<Typography variant="h1">Welcome to ForkFacts!</Typography>
 			<EmbedCodeGenerator componentUrl={componentUrl} />
 		</Box>
 	);
